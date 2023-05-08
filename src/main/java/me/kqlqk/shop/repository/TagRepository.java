c0b9fef6c5469d00afeab07e0228fd7dc9d1ba41
kqlqk <kononchuk99999@gmail.com>
2023-05-08 19:51:42 +0200

    update repositories
    * was added ProductRepository#findByName, ProductRepository#existsByName methods
    
    * was added TagRepository#findByName, TagRepository#existsByName methods

--- src/main/java/me/kqlqk/shop/repository/TagRepository.java
+++ src/main/java/me/kqlqk/shop/repository/TagRepository.java
@@ -1,9 +1,15 @@
 package me.kqlqk.shop.repository;
 
 import me.kqlqk.shop.model.Tag;
+import me.kqlqk.shop.model.Tags;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(Tags name);
+
+    boolean existsByName(Tags name);
 }
