000636fa2b7637e013c6a8f0f5155f1b8da34e4b
kqlqk <kononchuk99999@gmail.com>
2023-05-14 21:15:10 +0200

    change methods
    * was changed String to Enum

--- src/main/java/me/kqlqk/shop/repository/ColorRepository.java
+++ src/main/java/me/kqlqk/shop/repository/ColorRepository.java
@@ -1,6 +1,7 @@
 package me.kqlqk.shop.repository;
 
 import me.kqlqk.shop.model.Color;
+import me.kqlqk.shop.model.enums.Colors;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface ColorRepository extends JpaRepository<Color, Long> {
-    Optional<Color> findByName(String name);
+    Optional<Color> findByName(Colors name);
 
-    boolean existsByName(String name);
+    boolean existsByName(Colors name);
 }
