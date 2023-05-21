a69f370d48eb1e977b987f16e6986cff9153478a
kqlqk <kononchuk99999@gmail.com>
2023-05-22 00:54:04 +0200

    update test db

--- src/test/resources/AddData.sql
+++ src/test/resources/AddData.sql
@@ -1,13 +1,19 @@
 delete
-from product_tag;
+from product_color;
+delete
+from product_size;
 delete
 from product;
 delete
-from tag;
+from color;
+delete
+from size;
 
-ALTER SEQUENCE product_tag_id_seq RESTART WITH 1;
+ALTER SEQUENCE product_color_id_seq RESTART WITH 1;
+ALTER SEQUENCE product_size_id_seq RESTART WITH 1;
 ALTER SEQUENCE product_id_seq RESTART WITH 1;
-ALTER SEQUENCE tag_id_seq RESTART WITH 1;
+ALTER SEQUENCE color_id_seq RESTART WITH 1;
+ALTER SEQUENCE size_id_seq RESTART WITH 1;
 
 insert into product (name, price, path, description)
 values ('black t-shirt', 10, '/tshirts/1', 'some description');
@@ -15,17 +21,36 @@ values ('black t-shirt', 10, '/tshirts/1', 'some description');
 insert into product (name, price, path, description)
 values ('white oversize t-shirt', 20, '/tshirts/2', 'some description');
 
-insert into tag (name)
+
+insert into color (name)
 values ('BLACK');
 
-insert into tag (name)
-values ('XL');
+insert into color (name)
+values ('WHITE');
 
-insert into product_tag (product_id, tag_id)
+
+insert into size (name)
+values ('M');
+
+insert into size (name)
+values ('L');
+
+
+insert into product_color (product_id, color_id)
 values (1, 1);
 
-insert into product_tag (product_id, tag_id)
+insert into product_color (product_id, color_id)
 values (1, 2);
 
-insert into product_tag (product_id, tag_id)
+insert into product_color (product_id, color_id)
+values (2, 2);
+
+
+insert into product_size (product_id, size_id)
+values (1, 1);
+
+insert into product_size (product_id, size_id)
+values (1, 2);
+
+insert into product_size (product_id, size_id)
 values (2, 2);
