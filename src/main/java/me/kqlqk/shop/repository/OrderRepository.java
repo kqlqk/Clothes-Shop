c74ff55753382c72227e59adea875089be661ce2
kqlqk <kononchuk99999@gmail.com>
2023-06-03 10:38:27 +0200

    update repository layer
    * was added OrderRepository.java, UserRepository.java

---
+++ src/main/java/me/kqlqk/shop/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package me.kqlqk.shop.repository;
+
+import me.kqlqk.shop.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
