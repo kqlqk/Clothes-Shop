be508196d338e8e5fb8ba0330cf5f6b6de67ea3a
kqlqk <kononchuk99999@gmail.com>
2023-06-22 12:16:35 +0200

    add new table
    * was added RefreshToken.java

---
+++ src/main/java/me/kqlqk/shop/model/RefreshToken.java
@@ -0,0 +1,30 @@
+package me.kqlqk.shop.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "refresh_token")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", insertable = false, updatable = false)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "token", nullable = false, unique = true)
+    private String token;
+
+    public RefreshToken(User user, String token) {
+        this.user = user;
+        this.token = token;
+    }
+}
