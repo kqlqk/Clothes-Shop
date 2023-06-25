acecaa993f443b7d195d182165a5da2d2a831179
kqlqk <kononchuk99999@gmail.com>
2023-06-26 00:38:29 +0200

    add CustomAuthenticationEntryPoint.java
    * was added custom AuthenticationEntryPoint

---
+++ src/main/java/me/kqlqk/shop/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package me.kqlqk.shop.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        throw exception;
+    }
+}
