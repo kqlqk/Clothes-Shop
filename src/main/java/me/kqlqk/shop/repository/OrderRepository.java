e8ffaf0c2f6f8d860360bb1634ef84c2c365084d
kqlqk <kononchuk99999@gmail.com>
2023-07-24 15:19:47 +0200

    update repository layer
    * was added OrderRepository#findByUser, OrderRepository#findByUserAndRealisedOrderByCreateDate

--- src/main/java/me/kqlqk/shop/repository/OrderRepository.java
+++ src/main/java/me/kqlqk/shop/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package me.kqlqk.shop.repository;
 
 import me.kqlqk.shop.model.Order;
+import me.kqlqk.shop.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUser(User user);
+
+    List<Order> findByUserAndRealisedOrderByCreateDate(User user, boolean realised);
 }
