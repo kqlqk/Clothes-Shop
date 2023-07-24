e6a453fea8c5bc19224bacc1beff7a59eb10939a
kqlqk <kononchuk99999@gmail.com>
2023-07-24 15:26:22 +0200

    update service layer
    * was added OrderService#getByUser, OrderService#getByUserAndRealised and impls

--- src/main/java/me/kqlqk/shop/service/OrderService.java
+++ src/main/java/me/kqlqk/shop/service/OrderService.java
@@ -1,11 +1,18 @@
 package me.kqlqk.shop.service;
 
 import me.kqlqk.shop.model.Order;
+import me.kqlqk.shop.model.user.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface OrderService {
     Order getById(long id);
+
+    List<Order> getByUser(User user);
+
+    List<Order> getByUserAndRealised(User user, boolean realised);
 
     void add(Order order);
 
--- src/main/java/me/kqlqk/shop/service/impl/OrderServiceImpl.java
+++ src/main/java/me/kqlqk/shop/service/impl/OrderServiceImpl.java
@@ -4,10 +4,13 @@ import lombok.NonNull;
 import me.kqlqk.shop.exception.OrderExistsException;
 import me.kqlqk.shop.exception.OrderNotFoundException;
 import me.kqlqk.shop.model.Order;
+import me.kqlqk.shop.model.user.User;
 import me.kqlqk.shop.repository.OrderRepository;
 import me.kqlqk.shop.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -21,6 +24,28 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Order getById(long id) {
         return orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException("Order with id = " + id + " not found"));
+    }
+
+    @Override
+    public List<Order> getByUser(@NonNull User user) {
+        List<Order> orders = orderRepository.findByUser(user);
+
+        if (orders.isEmpty()) {
+            throw new OrderNotFoundException("Orders for user with userId = " + user.getId() + " not found");
+        }
+
+        return orders;
+    }
+
+    @Override
+    public List<Order> getByUserAndRealised(@NonNull User user, boolean realised) {
+        List<Order> orders = orderRepository.findByUserAndRealisedOrderByCreateDate(user, realised);
+
+        if (orders.isEmpty()) {
+            throw new OrderNotFoundException("Orders for user with userId = " + user.getId() + " not found");
+        }
+
+        return orders;
     }
 
     @Override
