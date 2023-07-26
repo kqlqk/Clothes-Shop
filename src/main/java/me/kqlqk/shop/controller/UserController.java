0749a0462be0e502e7580c67acca238f711e393f
kqlqk <kononchuk99999@gmail.com>
2023-07-26 20:54:55 +0200

    update UserController, UserPage.html
    * was updated UserController#getUserPage, UserController#editUserPage and representation UserPage.html

--- src/main/java/me/kqlqk/shop/controller/UserController.java
+++ src/main/java/me/kqlqk/shop/controller/UserController.java
@@ -2,17 +2,24 @@ package me.kqlqk.shop.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import me.kqlqk.shop.dto.AddressDTO;
+import me.kqlqk.shop.dto.CombinedDTO;
 import me.kqlqk.shop.dto.UserDTO;
 import me.kqlqk.shop.exception.OrderNotFoundException;
 import me.kqlqk.shop.model.Order;
 import me.kqlqk.shop.model.user.User;
 import me.kqlqk.shop.service.OrderService;
 import me.kqlqk.shop.service.UserService;
+import me.kqlqk.shop.util.Formatter;
 import me.kqlqk.shop.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -35,7 +42,8 @@ public class UserController {
     }
 
     @GetMapping
-    public String getUserPage(@PathVariable long id, Model model) {
+    public String getUserPage(@PathVariable long id, Model model,
+                              @RequestParam(value = "errors", required = false) String[] errors) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         List<Order> currentOrders;
         List<Order> realisedOrders;
@@ -58,25 +66,74 @@ public class UserController {
         Map<Integer, List<Order>> groupedRealisedOrders = realisedOrders.stream()
                 .collect(Collectors.groupingBy(Order::getUuid));
 
+        if (errors != null) {
+            model.addAttribute("errors", true);
+            for (String s : errors) {
+                String[] fieldValue = s.split("_");
+                fieldValue[0] = fieldValue[0].split("\\.")[1];
+
+                if (fieldValue.length > 1) {
+                    model.addAttribute(fieldValue[0], fieldValue[1]);
+                }
+            }
+        }
+
         model.addAttribute("currentOrders", groupedCurrentOrders.values());
         model.addAttribute("realisedOrders", groupedRealisedOrders.values());
-
-
         model.addAttribute("user", user);
-        model.addAttribute("userDTO", new UserDTO());
+        model.addAttribute("combinedDTO", new CombinedDTO());
 
         return "UserPage";
     }
 
     @PutMapping
-    public String editUserPage(@PathVariable long id, @ModelAttribute("userDTO") UserDTO userDTO,
+    public String editUserPage(@PathVariable long id,
+                               @Valid @ModelAttribute("combinedDTO") CombinedDTO combinedDTO,
+                               BindingResult bindingResult,
                                HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder("errors=");
+
+            List<ObjectError> errors = bindingResult.getAllErrors();
+            for (int i = 0; i < errors.size(); i++) {
+                sb.append(((FieldError) errors.get(i)).getField())
+                        .append("_")
+                        .append(errors.get(i).getDefaultMessage());
+
+                if (i + 1 != errors.size()) {
+                    sb.append("&errors=");
+                }
+            }
+
+            return "redirect:/user/" + id + "?" + sb;
+        }
+
+        if (combinedDTO.allFieldsAreNullOrBlank()) {
+            return "redirect:/user/" + id + "?errors";
+        }
+
+        String prefix = null;
+        UserDTO userDTO = combinedDTO.getUserDTO();
+        AddressDTO addressDTO = combinedDTO.getAddressDTO();
+
         User userDb = userService.getById(id);
         String oldEmail = userDb.getEmail();
         User user = new User();
         user.setId(id);
         user.setEmail(userDTO.getEmail());
         user.setName(userDTO.getName());
+        if (!
+                (userDb.getAddress() == null &&
+                        (addressDTO.getCountry() == null || addressDTO.getCountry().isBlank() ||
+                                addressDTO.getCity() == null || addressDTO.getCity().isBlank() ||
+                                addressDTO.getStreet() == null || addressDTO.getStreet().isBlank() ||
+                                addressDTO.getHouse() == null || addressDTO.getHouse().isBlank() ||
+                                addressDTO.getPostalCode() == null || addressDTO.getPostalCode().isBlank()))) {
+            user.setAddress(Formatter.convertToSave(addressDTO, userDb.getAddress()));
+        }
+        else {
+            prefix = "?errors=addressDTO.address_First time you should fill all address' form";
+        }
         userService.update(user);
 
         if (!oldEmail.equalsIgnoreCase(user.getEmail())) {
@@ -88,6 +145,6 @@ public class UserController {
             jwtUtil.updateRefreshTokenWithNewEmail(user.getEmail());
         }
 
-        return "redirect:/user/" + id;
+        return "redirect:/user/" + id + (prefix == null ? "" : prefix);
     }
 }
