368839915ec47e21e6094d7011c9b756daf4190b
kqlqk <kononchuk99999@gmail.com>
2023-07-30 20:08:41 +0200

    update repository layer
    * was added CardRepository.java

---
+++ src/main/java/me/kqlqk/shop/repository/CardRepository.java
@@ -0,0 +1,13 @@
+package me.kqlqk.shop.repository;
+
+import me.kqlqk.shop.model.Card;
+import me.kqlqk.shop.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+    List<Card> findByUser(User user);
+}
