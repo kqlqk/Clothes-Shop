16a5ef6a49045b151e9eb2c9d8d62236f539c81f
kqlqk <kononchuk99999@gmail.com>
2023-08-16 20:38:43 +0200

    update repository layer
    * was added ProductRepository#findSales, ProductRepository#findLastProducts

--- src/main/java/me/kqlqk/shop/repository/ProductRepository.java
+++ src/main/java/me/kqlqk/shop/repository/ProductRepository.java
@@ -2,8 +2,11 @@ package me.kqlqk.shop.repository;
 
 import me.kqlqk.shop.model.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +16,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByName(String name);
 
     boolean existsByPath(String path);
+
+    @Query("from Product p where p.discount > 0")
+    List<Product> findSales();
+
+    @Query(value = "select * from Product p " +
+            "order by p.id desc " +
+            "limit :limit",
+            nativeQuery = true)
+    List<Product> findLastProducts(@Param("limit") int limit);
 }
